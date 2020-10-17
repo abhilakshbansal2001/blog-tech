1b81bcf9c6bcf0e8c790659c1bd414980ab0121f
abhilakshbansal2001 <abhilakshbansal2001@gmail.com>
2020-10-17 17:04:28 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -4,28 +4,17 @@ const About = ({ title, description, ...props }) => {
   return (
     <>
       <Layout pageTitle={`${title} | About`} description={description}>
-        <h1 className="title">Welcome to this demo blog!</h1>
+        <h1 className="title">Welcome to Abhilaksh site !</h1>
 
         <p className="description">
-          This is a simple blog built with Next, easily deployable on{' '}
-          <a href="https://url.netlify.com/r1j6ybSYU">Netlify</a>.
+          This is a simple website built on next js.
+        
         </p>
 
+       
         <p>
-          You can check out the{' '}
-          <a href="https://github.com/cassidoo/next-netlify-blog-starter">
-            repo here.
-          </a>{' '}
-          If you'd like to build it yourself,{' '}
-          <a href="https://url.netlify.com/ByVW0bCF8">
-            here is a tutorial on how to do so
-          </a>
-          !
-        </p>
-
-        <p>
-          This project includes a basic layout and header, base styles, dynamic
-          routing with getStaticPaths, and posts saved as Markdown.
+          See my portfolio
+          <a href=https://abhilakshbansal2001.github.io/abhilakshbansal/"">Abhilaksh Bansal</a>
         </p>
       </Layout>
     </>
